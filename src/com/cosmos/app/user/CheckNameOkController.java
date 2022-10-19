e46a7a1489c3ffd0c77f44fe9467f07d78259e1e
redhawk90 <slions90@naver.com>
2022-10-19 14:35:26 +0900

    성윤 로그인 회원가입 ver1.1

--- src/com/cosmos/app/user/CheckNameOkController.java
+++ src/com/cosmos/app/user/CheckNameOkController.java
@@ -7,14 +7,32 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
 
 import com.cosmos.app.Execute;
 import com.cosmos.app.Result;
+import com.cosmos.app.user.dao.UserDAO;
 
 public class CheckNameOkController implements Execute{
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userNickname = req.getParameter("userNickname");
+		new UserDAO().checkNickName(userNickname);  
+		PrintWriter out = resp.getWriter();
+		JSONObject jsonObject = new JSONObject(); 
+		
+//		jsonObject.put("result", new UserDAO().checkNickName(userNickname));
+//		out.print(jsonObject.toString());
+			
+		try {
+			jsonObject.put("result", new UserDAO().checkNickName(userNickname));
+			out.print(jsonObject.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		out.close();
 		return null;
 	}
 	
